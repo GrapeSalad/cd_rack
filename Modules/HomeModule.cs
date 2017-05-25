520f341f3d53d99dde8b98548242ffc9292d7c80
David Wilson and Pete Lazuran <david+pete@>
2017-05-25 14:40:54 -0700

    make url pathing more RESTful

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,10 +9,14 @@ namespace Organizer
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["index.cshtml"];
-      Get["/add_cd"] = _ => View["add_cd_form.cshtml"];
+      // Get["/"] = _ => View["index.cshtml"];
+      Get["/"] = _ => {
+        List<Rack> fullRack = Rack.GetAll();
+        return View["view_cd_titles.cshtml", fullRack];
+      };
+      Get["/cd/add"] = _ => View["add_cd_form.cshtml"];
 
-      Post["add_cd"] = _ => {
+      Post["/cd/cds"] = _ => {
         Rack newRack = new Rack(Request.Form["title"]);
         List<Rack> fullRack = Rack.GetAll();
         return View["view_cd_titles.cshtml", fullRack];
@@ -26,7 +30,7 @@ namespace Organizer
         return View["cd_info.cshtml", model];
       };
 
-      Get["/cd/{id}/add_artist_form"] = parameters => {
+      Get["/cd/{id}/artist/add"] = parameters => {
         Dictionary<string,object> model = new Dictionary<string, object>();
         Rack selectedRack = Rack.Find(parameters.id);
         List<Artist> cdArtists = selectedRack.GetArtists();
@@ -34,7 +38,7 @@ namespace Organizer
         model.Add("name", cdArtists);
         return View["artist_entry_form.cshtml", model];
       };
-      Post["/add_artist_name"] = _ =>{
+      Post["/cd/{id}/artists"] = _ =>{
         Dictionary<string,object> model = new Dictionary<string, object>();
         Rack selectedRack = Rack.Find(Request.Form["rack-id"]);
         // Console.WriteLine(selectedRack.GetTitle());
@@ -51,10 +55,6 @@ namespace Organizer
         return View["cd_info.cshtml", model];
       };
 
-      Get["/cd"] = _ => {
-        List<Rack> fullRack = Rack.GetAll();
-        return View["view_cd_titles.cshtml", fullRack];
-      };
     }
   }
 }
